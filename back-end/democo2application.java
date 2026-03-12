67812237410cb1686095067b84946fc5db1026ed
MudaserShahrzad <2411983@brunel.ac.uk>
2026-03-12 02:32:33 +0000

    Initial Spring Boot project setup
    Created the base Spring Boot project using Maven. Added core dependencies including Spring Web and Spring Data JPA to support REST API development and database integration.

--- back-end/democo2application.java
+++ back-end/democo2application.java
@@ -3,14 +3,11 @@ package a2.startup2.co2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Co2Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Co2Application.class, args);
-  }
-
-  
+    public static void main(String[] args) {
+        SpringApplication.run(Co2Application.class, args);
+    }
 
 }
